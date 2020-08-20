0f14693eab1b753bd9b026de4271f1fe73568793
Pravat45 <pravat.behera45@gmail.com>
2020-08-20 17:17:34 +0530

    first commit

---
+++ src/Hello.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+import "./Hello.css";
+class Hello extends Component {
+  render() {
+    return (
+      <div className="f1 tc">
+        <h1>Pravat Kumar Behera</h1>
+        <p>I am a MERN stack Developer</p>
+      </div>
+    );
+  }
+}
+export default Hello;
---
+++ src/robots.js
@@ -0,0 +1,85 @@
+export const robots = [
+  {
+    id: 1,
+    name: "Pravat Kumar Behera",
+    email: "pravat@gmail.com",
+  },
+
+  {
+    id: 2,
+    name: " Tarique Akhtar",
+    email: "tarique@gmail.com",
+  },
+
+  {
+    id: 3,
+    name: "Santosh Das",
+    email: "Santsoh@gmail.com",
+  },
+
+  {
+    id: 4,
+    name: "Sandeep Das",
+    email: "sandeep@gmail.com",
+  },
+
+  {
+    id: 5,
+    name: "Shouvik Sinha",
+    email: "shouvik@gmail.com",
+  },
+  {
+    id: 6,
+    name: "Clementine Bauch",
+    username: "Samantha",
+    email: "Nathan@yesenia.net",
+  },
+  {
+    id: 7,
+    name: "Patricia Lebsack",
+    username: "Karianne",
+    email: "Julianne.OConner@kory.org",
+  },
+  {
+    id: 8,
+    name: "Chelsey Dietrich",
+    username: "Kamren",
+    email: "Lucio_Hettinger@annie.ca",
+  },
+  {
+    id: 9,
+    name: "Mrs. Dennis Schulist",
+    username: "Leopoldo_Corkery",
+    email: "Karley_Dach@jasper.info",
+  },
+  {
+    id: 10,
+    name: "Kurtis Weissnat",
+    username: "Elwyn.Skiles",
+    email: "Telly.Hoeger@billy.biz",
+  },
+  {
+    id: 11,
+    name: "Nicholas Runolfsdottir V",
+    username: "Maxime_Nienow",
+    email: "Sherwood@rosamond.me",
+  },
+  {
+    id: 12,
+    name: "Glenna Reichert",
+    username: "Delphine",
+    email: "Chaim_McDermott@dana.io",
+  },
+  {
+    id: 13,
+    name: "Clementina DuBuque",
+    username: "Moriah.Stanton",
+    email: "Rey.Padberg@karina.biz",
+  },
+  {
+    id: 14,
+    name: "biplab swain ",
+    username: "Moriah.Stanton",
+    email: "Rey.Padberg@karina.biz",
+  },
+];
